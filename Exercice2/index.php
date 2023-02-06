5f96acb7f689bcb2eedbdea40fddc4df9ba66f70
Adjoumani <danielkouadio182@gmail.com>
2023-02-06 10:14:43 +0000

    First Commit

---
+++ Exercice2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<br><br>
+<h1 style="text-align: center;">ExerciceXP 2 Calculate Area Of Rectangle</h1>
+
+<div class="container"><br>
+
+        <?php
+                $fruits = array("Janvier <br>", "Fevrier <br>", "Mars <br>", "Avril <br>", "Mai <br>", "Juin <br>", "Juillet <br>", "Août <br>", "Septembre <br>", "Octobre <br>", "Novembre <br>", "Décembre <br>");
+                        unset($fruits[2]);
+                        print_r($fruits); 
+        ?>
+</div>
+
+</body>
+</html>
